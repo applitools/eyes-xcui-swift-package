24b24dab229baf9db2a22efec495fe1436bd8761
Anton Chuev <anton.chuev@applitools.com>
2022-09-09 16:48:15 +0300

    Remove package to folder with -beta suffix

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "EyesXCUI",
-                      url: "https://applitools.jfrog.io/artifactory/iOS/EyesXCUISwiftPackage/8.11.0/EyesXCUI.xcframework.zip",
+                      url: "https://applitools.jfrog.io/artifactory/iOS/EyesXCUISwiftPackage/8.11.0-betaß/EyesXCUI.xcframework.zip",
                       checksum: "8722e732396b531f36a81f5e3cc040d7e9612ca059c01faead83ea6dda221f69"),
     ]
 )
