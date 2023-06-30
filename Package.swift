62a6ddcf8219fc9d21fd9f922f0ffe8cbb724d09
Clement <clement.barry@applitools.com>
2023-06-30 06:45:54 +0300

    v8.15.1 built with Xcode 14.2 for NML

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "EyesXCUI",
-                      url: "https://applitools.jfrog.io/artifactory/iOS/EyesXCUISwiftPackage/8.14.1-beta/EyesXCUI.xcframework.zip",
-                      checksum: "e4bee5fb285e54b41543cf7708e5d97cba146005fe7049c3215ba2c93db1ec39"),
+                      url: "https://applitools.jfrog.io/artifactory/iOS/EyesXCUISwiftPackage/8.15.1-beta/EyesXCUI.xcframework.zip",
+                      checksum: "58b90aa83be0b1ba1a5c76b8b9c59c3714cb84f39db5f3a4f81e3c0f1d558ea8"),
     ]
 )
