37a725b7f443f710b5030eaf826610a7b601f2e6
Clement <clement.barry@applitools.com>
2023-07-13 13:19:29 +0300

    v9.0.0

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "EyesXCUI",
-                      url: "https://applitools.jfrog.io/artifactory/iOS/EyesXCUISwiftPackage/8.12.1/EyesXCUI.xcframework.zip",
-                      checksum: "e3c9a8a42bcc4cad56fe360b83486580afd7893334dddedbbe21c69ad6eb9018"),
+                      url: "https://applitools.jfrog.io/artifactory/iOS/EyesXCUISwiftPackage/9.0.0/EyesXCUI.xcframework.zip",
+                      checksum: "159fec50f9b8d74d93f3ea942fc7a797d3399be25e04c65a4ae6daa9f8fee010"),
     ]
 )
