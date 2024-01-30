8e8f8fb2474856e7184adf9d6b06d35aa0541294
Clement <clement.barry@applitools.com>
2024-01-30 13:00:21 +0200

    v9.3.2

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "EyesXCUI",
-                      url: "https://github.com/applitools/eyes-xcui-swift-package/releases/download/9.3.1/EyesXCUI.xcframework.zip",
-                      checksum: "c63551f3344ebe65bd5420c511028d0c783d14423812bdacc06362f3930e92f2"),
+                      url: "https://github.com/applitools/eyes-xcui-swift-package/releases/download/9.3.2/EyesXCUI.xcframework.zip",
+                      checksum: "5da08b396b946090f9aff95d3735146a544e13a41610197514f18ab332f0cb42"),
     ]
 )
