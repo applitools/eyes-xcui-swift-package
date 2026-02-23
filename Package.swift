d302ffea88dbd9a0ceb08a8d0bc691c6c2703254
Clement <clement.barry@applitools.com>
2026-02-23 15:13:06 +0200

    v10.1.5
    arm64e

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "EyesXCUI",
-                      url: "https://sdksstorage.blob.core.windows.net/mobile/ios/eyesxcui/10.1.4/EyesXCUI.zip",
-                      checksum: "ac450a5ca85df71b03577f0c487ac014915a6d978b05026b6b70e5a4a4f87213"),
+                      url: "https://sdksstorage.blob.core.windows.net/mobile/ios/eyesxcui-test/10.1.5/EyesXCUI.zip",
+                      checksum: "c500ad39a839897b9128c3da6ceb72bc39bd0a9e701499cb580fd1cf970a98b0"),
     ]
 )
